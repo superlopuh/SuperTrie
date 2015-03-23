fa6eb670cff5f92249b8d41e57b92d89fc2d8379
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-03-23 14:21:52 +0000

    created XCode project

---
+++ SuperTrie/SuperTrie.h
@@ -0,0 +1,19 @@
+//
+//  SuperTrie.h
+//  SuperTrie
+//
+//  Created by Alexandre Lopoukhine on 23/03/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SuperTrie.
+FOUNDATION_EXPORT double SuperTrieVersionNumber;
+
+//! Project version string for SuperTrie.
+FOUNDATION_EXPORT const unsigned char SuperTrieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperTrie/PublicHeader.h>
+
+
