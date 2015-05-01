2aa81ce06223fa4729283bd243fa8a4d6b797c9d
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-05-01 11:57:23 +0100

    deleted useless STTrie, all you really need is the STNode

--- SuperTrie/STNode.swift
+++ SuperTrie/STNode.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 bluetatami. All rights reserved.
 //
 
-import Foundation
-
 public final class STNode<N, E: Hashable> {
     typealias EdgeValueType = E
     typealias NodeValueType = N
@@ -16,7 +14,37 @@ public final class STNode<N, E: Hashable> {
     public var nodeValue: N?
     public var nextNodeDict: [E:STNode<N,E>] = [:]
     
-    init(value: N?) {
+    init(value: N? = nil) {
         self.nodeValue = value
+    }
+    
+    public init<W: SequenceType, WPS: SequenceType where W.Generator.Element == E, WPS.Generator.Element == (N,W)>(wordPairs: WPS) {
+        addWordPairs(wordPairs)
+    }
+    
+    public func addWordPairs<W: SequenceType, WPS: SequenceType where W.Generator.Element == E, WPS.Generator.Element == (N,W)>(wordPairs: WPS) {
+        for (nodeValue, word) in wordPairs {
+            addWord(word, nodeValue: nodeValue)
+        }
+    }
+    
+    public func addWord<W : SequenceType where W.Generator.Element == E>(word: W, nodeValue: N) {
+        addWord(word.generate(), nodeValue: nodeValue)
+    }
+    
+    public func addWord<WG: GeneratorType where WG.Element == E>(var wordGenerator: WG, nodeValue: N) {
+        if let nextCharacter = wordGenerator.next() {
+            if let child = nextNodeDict[nextCharacter] {
+                // Create what is left of the word to the child
+                child.addWord(wordGenerator, nodeValue: nodeValue)
+            } else {
+                // Create and add child
+                let newNode = STNode<N,E>()
+                nextNodeDict[nextCharacter] = newNode
+            }
+        } else {
+            // Add empty string, or, make accepting
+            self.nodeValue = nodeValue
+        }
     }
 }
